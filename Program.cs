08181875e40b25374f08fab0eab31fa58e5bafe0
Bruno Borges <76704076+DEV-Borges@users.noreply.github.com>
2024-08-28 11:31:18 -0300

    Conclusão do desafio.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "12345678910",modelo:"Modelo 1", imei: "111111111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone Iphone = new Iphone(numero: "12345678910",modelo:"Modelo 1", imei: "111111111111111", memoria: 128);
+Iphone.ReceberLigacao();
+Iphone.InstalarAplicativo("Instagram");
